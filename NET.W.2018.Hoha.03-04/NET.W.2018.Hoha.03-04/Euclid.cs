19c8ad683d7352aae2821557ee9f81ead936699f
AlexHoha <38506232+AlexHoha@users.noreply.github.com>
2018-10-20 15:24:53 +0300

    Add files via upload

---
+++ NET.W.2018.Hoha.03-04/NET.W.2018.Hoha.03-04/Euclid.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+namespace NET.W._2018.Hoha._03_04
+{
+    public class Euclid
+    {
+        static Stopwatch sw = new Stopwatch();
+        public Euclid()
+        {
+        }
+
+        static int FindGCD(int a, int b)
+        {
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            if (a == 0 || b == 0)
+                throw new ArgumentException();
+            while(a != b)
+            {
+                if(a > b)
+                {
+                    a -= b;
+                }
+                else
+                {
+                    b -= a;
+                }
+            }
+            return a;
+        }
+
+        public static int FindGCD(int[] input, out TimeSpan ts)
+        {
+            sw.Start();
+            int tempGCD = 0;
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                tempGCD = FindGCD(input[i], input[i+1]);
+            }
+            sw.Stop();
+            ts = sw.Elapsed;
+            return tempGCD;
+        } 
+    }
+}
---
+++ NET.W.2018.Hoha.03-04/NET.W.2018.Hoha.03-04/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NET.W._2018.Hoha._03_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = { 16,32};
+            TimeSpan ts = new TimeSpan();
+            Console.WriteLine(Euclid.FindGCD(input,out ts));
+            Console.WriteLine(ts);
+        }
+    }
+}
